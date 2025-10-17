91d969d80ed42457d183767f099e1f7b4f55190e
Momna <momnashafqaat@gmail.com>
2025-10-17 13:39:41 +0500

    media files fix

--- waf_project/urls.py
+++ waf_project/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('data_management.urls')),  # uploaded files API
     path('api/', include('waf_analysis.urls')),    # rule relationships API
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
