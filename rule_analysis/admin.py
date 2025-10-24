512de8764c9c226261c098fcb4cb92f5e5e66b3a
momna-ahmad <momnaahmdd@gmail.com>
2025-10-24 12:54:02 +0500

    Revert "Merge pull request #1 from MomnaShafqaat/threshold-module"
    This reverts commit 4e74ec9056ad195695a2e3b2cc3df0fc282b8a2f, reversing
    changes made to 9aee15f348a0a90f7df2f9b8589068799b05266d.
    
    Revert "Merge pull request #42 from feature/login"

--- rule_analysis/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ThresholdSuggestion
-
-@admin.register(ThresholdSuggestion)
-class ThresholdSuggestionAdmin(admin.ModelAdmin):
-    list_display = ('value', 'approved', 'created_at')
-    list_filter = ('approved',)
--- waf_project/urls.py
+++ waf_project/urls.py
@@ -2,15 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rule_analysis.views import threshold_tuning_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('data_management.urls')),
    # path('api/', include('waf_analysis.urls')),
-    path('api/', include('rule_analysis.urls')),
-    path('api/threshold_tuning/', threshold_tuning_view),
- 
+    path('api/', include('rule_analysis.urls')), 
 ]
 
 # Serve media files in development
